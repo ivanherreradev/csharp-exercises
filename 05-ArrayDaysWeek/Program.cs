778b17f2094fabe6492ad046edcadc41ab3d98b0
ivanherreradev <ivanalejandroherrera00@gmail.com>
2025-05-09 22:28:04 -0300

    add 05-ArrayDaysWeek

---
+++ 05-ArrayDaysWeek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Crea un array y rellénalo con los días de la semana. Luego pide al usuario
+    que introduzca un número del 1 al 7 para saber qué día de la semana tiene
+    libre en su trabajo y dependiendo del número introducido, debes mostrar
+    por consola el día de la semana correspondiente.
+
+    Nota: Recuerda que las posiciones del array comienzan a partir del cero.
+*/
+
+namespace ArrayDaysWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            bool canContinue = true;
+
+            while(canContinue)
+            {
+                Console.Write("Enter a number from 1 to 7 to display the corresponding day of the week: ");
+                int day = int.Parse(Console.ReadLine());
+                
+                if (day > 0 && day < 8)
+                {
+                    Console.WriteLine(weekDays[day-1]);
+                    canContinue = false;
+                }
+            }
+        }
+    }
+}
